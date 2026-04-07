11288f6a24b84ee1567d3ab27f9be6d84f573ee8
CorbinAngel <corbinangelukk@gmail.com>
2026-04-08 03:41:41 +0500

    Update SellResult.java

--- src/main/java/net/mackenziemolloy/shopguiplus/sellgui/objects/SellResult.java
+++ src/main/java/net/mackenziemolloy/shopguiplus/sellgui/objects/SellResult.java
@@ -2,8 +2,26 @@ package net.mackenziemolloy.shopguiplus.sellgui.objects;
 
 import org.bukkit.inventory.ItemStack;
 
-public record SellResult(
-        double totalPrice,
-        int itemAmount,
-        ItemStack shulker
-) {}
+public class SellResult {
+    private final double totalPrice;
+    private final int itemAmount;
+    private final ItemStack shulker;
+
+    public SellResult(double totalPrice, int itemAmount, ItemStack shulker) {
+        this.totalPrice = totalPrice;
+        this.itemAmount = itemAmount;
+        this.shulker = shulker;
+    }
+
+    public double totalPrice() {
+        return totalPrice;
+    }
+
+    public int itemAmount() {
+        return itemAmount;
+    }
+
+    public ItemStack shulker() {
+        return shulker;
+    }
+}
